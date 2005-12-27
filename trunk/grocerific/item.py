56d1769a3a0f55cecf565477e32e89a0adbc9c9f
Doug Hellmann <doug@doughellmann.com>
2005-12-27 17:37:58 +0000

    Add comments and handle blank input query strings.
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/grocerific@27 ad7e9fe2-ab08-0410-a543-8a0b6c3202fc

--- trunk/grocerific/item.py
+++ trunk/grocerific/item.py
@@ -30,17 +30,32 @@ class ItemManager:
 
     @turbogears.expose(format="xml", content_type="text/xml")
     def findItems(self, queryString=None, **args):
-
+        #
+        # Clean up the string we are given and turn it
+        # into words that might appear in the name
+        # of a shopping item.
+        #
         clean_query_string = cleanString(queryString)
         words = clean_query_string.split(' ')
         where_clauses = []
         for word in words:
+            word = word.strip()
+            if not word:
+                continue
             where_clauses.append("shopping_item.name LIKE '%%%s%%'" % word)
-        
-        select_string = ' AND '.join(where_clauses)
-        print select_string
-        items = ShoppingItem.select(select_string)
 
+        #
+        # Assemble the select string and get the items.
+        #
+        if where_clauses:
+            select_string = ' AND '.join(where_clauses)
+            items = ShoppingItem.select(select_string)
+        else:
+            items = []
+
+        #
+        # Format the response table
+        #
         response_text = ''
         for item in items:
             response_text += '<tr><td>%s</td></tr>' % item.name

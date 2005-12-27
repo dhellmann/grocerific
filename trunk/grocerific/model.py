a26012c6fd2109e898065c1cec3cd62fd46f5665
Doug Hellmann <doug@doughellmann.com>
2005-12-27 17:32:13 +0000

    Add comments.
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/grocerific@26 ad7e9fe2-ab08-0410-a543-8a0b6c3202fc

--- trunk/grocerific/model.py
+++ trunk/grocerific/model.py
@@ -1,10 +1,35 @@
+#
+# $Id$
+#
+# Copyright (c) 2005 Racemi, Inc.  All rights reserved.
+#
+
+"""Database model.
+
+"""
+
+#
+# Import system modules
+#
 from sqlobject import *
 from turbogears.database import PackageHub
+
+#
+# Import Local modules
+#
+
+
+#
+# Module
+#
 
 hub = PackageHub("grocerific")
 __connection__ = hub
 
+
 class User(SQLObject):
+    """Users of the site.
+    """
     class sqlmeta:
         # 'user' is a reserved word in some databases,
         # so give a different name for the table.
@@ -14,10 +39,16 @@ class User(SQLObject):
     email = StringCol()
     lists = MultipleJoin('ShoppingList')
 
+    
 class ShoppingItem(SQLObject):
+    """Items someone can purchase.
+    """
     name = StringCol(alternateID=True)
 
+    
 class ShoppingList(SQLObject):
+    """Lists of things users have indicated that they want to buy.
+    """
     name = StringCol()
     user = ForeignKey('User')
 
@@ -26,6 +57,8 @@ class ShoppingList(SQLObject):
                                          orderBy='item.name')
 
 class ShoppingListItem(SQLObject):
+    """Items someone has indicated that they may buy.
+    """
     item = ForeignKey('ShoppingItem')
     list = ForeignKey('ShoppingList')
     quantity = StringCol()

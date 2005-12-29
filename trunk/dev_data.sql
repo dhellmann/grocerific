0c7c852f54eaf0e0aeb95d4c63249856339fb370
Doug Hellmann <doug@doughellmann.com>
2005-12-29 14:50:55 +0000

    Add more default data to the database.
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/grocerific@39 ad7e9fe2-ab08-0410-a543-8a0b6c3202fc

--- trunk/dev_data.sql
+++ trunk/dev_data.sql
@@ -11,20 +11,35 @@ INSERT INTO siteuser (username, password, email) VALUES ('doug', 'ntsucks', 'dou
 INSERT INTO shopping_list (name, user_id) VALUES ('Next Trip', 1);
 
 -- Add items we can put in the list
-INSERT INTO shopping_item (name) VALUES ( 'Carrots, large');
-INSERT INTO shopping_item (name) VALUES ( 'Onion, yellow, small');
-INSERT INTO shopping_item (name) VALUES ( 'Onion, yellow, medium');
-INSERT INTO shopping_item (name) VALUES ( 'Onion, yellow, large');
-INSERT INTO shopping_item (name) VALUES ( 'Onion, red, small');
-INSERT INTO shopping_item (name) VALUES ( 'Onion, red, medium');
-INSERT INTO shopping_item (name) VALUES ( 'Onion, red, large');
-INSERT INTO shopping_item (name) VALUES ( 'Milk, skim, 1/2 gallon');
-INSERT INTO shopping_item (name) VALUES ( 'Milk, skim, gallon');
-INSERT INTO shopping_item (name) VALUES ( 'Apple, Gala');
-INSERT INTO shopping_item (name) VALUES ( 'Apple, Red Delicious');
-INSERT INTO shopping_item (name) VALUES ( 'Apple, Granny Smith');
-INSERT INTO shopping_item (name) VALUES ( 'Apple, Washington');
-INSERT INTO shopping_item (name) VALUES ( 'Apple, Fuji');
+INSERT INTO "shopping_item" VALUES(1, 'Carrots, large');
+INSERT INTO "shopping_item" VALUES(2, 'Onion, yellow, small');
+INSERT INTO "shopping_item" VALUES(3, 'Onion, yellow, medium');
+INSERT INTO "shopping_item" VALUES(4, 'Onion, yellow, large');
+INSERT INTO "shopping_item" VALUES(5, 'Onion, red, small');
+INSERT INTO "shopping_item" VALUES(6, 'Onion, red, medium');
+INSERT INTO "shopping_item" VALUES(7, 'Onion, red, large');
+INSERT INTO "shopping_item" VALUES(8, 'Milk, skim, 1/2 gallon');
+INSERT INTO "shopping_item" VALUES(9, 'Milk, skim, gallon');
+INSERT INTO "shopping_item" VALUES(10, 'Apple, Gala');
+INSERT INTO "shopping_item" VALUES(11, 'Apple, Red Delicious');
+INSERT INTO "shopping_item" VALUES(12, 'Apple, Fuji');
+INSERT INTO "shopping_item" VALUES(13, 'Flour, All-purpose');
+INSERT INTO "shopping_item" VALUES(14, 'Flour, bread');
+INSERT INTO "shopping_item" VALUES(15, 'Cheese, deli, provolone');
+INSERT INTO "shopping_item" VALUES(16, 'Cheese, deli, American');
+INSERT INTO "shopping_item" VALUES(17, 'Cheese, deli, cheddar');
+INSERT INTO "shopping_item" VALUES(18, 'Ham, deli');
+INSERT INTO "shopping_item" VALUES(19, 'Cheese, deli');
+INSERT INTO "shopping_item" VALUES(20, 'Cheese, cheddar');
+INSERT INTO "shopping_item" VALUES(21, 'Tomato');
+INSERT INTO "shopping_item" VALUES(22, 'Tomato, canned, crushed');
+INSERT INTO "shopping_item" VALUES(23, 'Tomato, canned, whole');
+INSERT INTO "shopping_item" VALUES(24, 'Tomato, canned, diced');
+INSERT INTO "shopping_item" VALUES(25, 'Tomato, canned, Ro-Tel');
+INSERT INTO "shopping_item" VALUES(26, 'Velveeta, low-fat, small');
+INSERT INTO "shopping_item" VALUES(27, 'Velveeta, Mexican, small');
+INSERT INTO "shopping_item" VALUES(28, 'Cat chow');
+INSERT INTO "shopping_item" VALUES(29, 'Cheese, deli, swiss');
 
 -- Add items to the list
 INSERT INTO shopping_list_item (list_id, item_id, quantity) VALUES ( 1, 1, '3' );

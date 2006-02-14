ac4f211a683a9de8e425ed74a7d202d8300ae096
Doug Hellmann <doug@doughellmann.com>
2006-02-14 22:37:19 +0000

    remove branch
    git-svn-id: file:///Users/dhellmann/Devel/svn-conversion/SVNRepository/grocerific@169 ad7e9fe2-ab08-0410-a543-8a0b6c3202fc

--- branches/experimental-branch/backupdb.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# $Id$
-#
-# Back up the database.
-#
-# We only want the INSERT statements (not the CREATE TABLE statements)
-#
-echo '.dump' | sqlite3 grocerific.db | grep "INSERT INTO" > dev_data.sql
